b29ac5d3534665fa91b4fde8dc4984532a9e35b0
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-09-15 15:36:09 +0200

    Let's use underscores instead of dashes.

--- jobs/fitbit_leaderboard.rb
+++ jobs/fitbit_leaderboard.rb
@@ -2,8 +2,8 @@ fitbit = Fitbit.new
 
 SCHEDULER.every "15m", first_in: 0 do |job|
   if fitbit.errors?
-    send_event "fitbit-leaderboard", { error: fitbit.error }
+    send_event "fitbit_leaderboard", { error: fitbit.error }
   else
-    send_event "fitbit-leaderboard", { people: fitbit.leaderboard }
+    send_event "fitbit_leaderboard", { people: fitbit.leaderboard }
   end
 end
